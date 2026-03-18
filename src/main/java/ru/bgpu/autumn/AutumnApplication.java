76f815391b7a6b62894bf140043fdf7ee82b62eb
out <piligrim.a3@gmail.com>
2026-03-18 12:29:33 +0900

    init

---
+++ src/main/java/ru/bgpu/autumn/AutumnApplication.java
@@ -0,0 +1,13 @@
+package ru.bgpu.autumn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutumnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutumnApplication.class, args);
+    }
+
+}
