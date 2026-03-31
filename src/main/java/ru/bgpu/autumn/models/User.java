470364399bece443da2e16f3937a0d3c8a439e34
out <piligrim.a3@gmail.com>
2026-03-31 12:28:19 +0900

    add: JPA

---
+++ src/main/java/ru/bgpu/autumn/models/User.java
@@ -0,0 +1,50 @@
+package ru.bgpu.autumn.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String login;
+
+    public User() {
+    }
+
+    public User(String name, String login) {
+        this.name = name;
+        this.login = login;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
