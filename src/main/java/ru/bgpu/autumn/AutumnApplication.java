871b4b663b0b51428644d95b0e813864a8a4e98b
quosha <onoe0201@gmail.com>
2026-06-01 14:36:02 +0300

    Add @EntityScan for warehouse.model package

--- src/main/java/ru/bgpu/autumn/AutumnApplication.java
+++ src/main/java/ru/bgpu/autumn/AutumnApplication.java
@@ -2,10 +2,12 @@ package ru.bgpu.autumn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"ru.bgpu.autumn", "com.warehouse"})
 @EnableJpaRepositories(basePackages = {"ru.bgpu.autumn.repositories", "com.warehouse.repository"})
+@EntityScan(basePackages = {"ru.bgpu.autumn.models", "com.warehouse.model"})
 public class AutumnApplication {
 
     public static void main(String[] args) {
